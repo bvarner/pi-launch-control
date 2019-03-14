2038b909e2cd676c356ba8b4207eb43b8c6b1d15
bvarner <bryan@varnernet.com>
2019-03-14 15:47:02 -0400

    frontend updates

--- pi-launch-control/webroot/App.js
+++ pi-launch-control/webroot/App.js
@@ -1,6 +1,4 @@
-"use strict";
-
-
+import ScalePanel from './ScalePanel.js';
 
 class App {
     constructor() {
---
+++ pi-launch-control/webroot/ScalePanel.js
@@ -0,0 +1,40 @@
+import {html, render} from 'https://unpkg.com/lit-html?module';
+
+export default class ScalePanel extends HTMLElement {
+    constructor() {
+        super();
+
+        this.root = this.attachShadow({mode: 'open'});
+        this.scaleReading = "unknown";
+
+        this.eventSource = new EventSource('/events');
+    }
+
+    connectedCallback() {
+        this.render();
+        this.eventSource.addEventListener('ScaleSample', e => this.onScaleSample(e))
+    }
+
+    onScaleSample(evt) {
+        var sample = JSON.parse(evt.data);
+        this.scaleReading = sample.Volt0;
+
+        this.render();
+    }
+
+    render() {
+        const reading = this.scaleReading;
+        const template = html`
+            <section>
+                <div id="chart"></div>
+                <div>
+                    <label>${reading}</label>
+                </div>
+            </section>
+        `;
+
+        render(template, this.root);
+    }
+}
+
+customElements.define("scale-panel", ScalePanel);
